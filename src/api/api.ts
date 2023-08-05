6d3a5681f8387ca8869f2ae63e1a03975823eac0
PaolaBravo1 <paolasuxobravo@gmail.com>
2023-08-05 15:39:04 -0500

    Fixed api root url variablization

--- src/api/api.ts
+++ src/api/api.ts
@@ -19,11 +19,11 @@ let apiRootUrl = configSettings.ApiRootUrl;
 console.log("hostname: " + window.location.hostname);
 
 if (window.location.hostname.startsWith("web-01"))
-  apiRootUrl = "hammerbeam-api-01.azurewebsites.net";
+  apiRootUrl = "https://hammerbeam-api-01.azurewebsites.net";
 else if (window.location.hostname.startsWith("web-02"))
-  apiRootUrl = "hammerbeam-api-02.azurewebsites.net";
+  apiRootUrl = "https://hammerbeam-api-02.azurewebsites.net";
 else if (window.location.hostname.startsWith("web-03"))
-  apiRootUrl = "hammerbeam-api-03.azurewebsites.net";
+  apiRootUrl = "https://hammerbeam-api-03.azurewebsites.net";
 
 console.log("apirooturl: " + apiRootUrl);
 
